a11cb956a326c48453ad9ce240b7c6edce06d0cb
Dan Boniface <DBoniface@victaulic.com>
2021-11-08 14:20:06 -0500

    adding album type

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import "rxjs/add/operator/map"
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -9,8 +10,8 @@ export class ProductService {
   private _albumUrl = "../assets/album.json"
   constructor(private _http: Http) { }
 
-  getAlbum(id: number): Observable<void> {
-    return this._http.get(this._albumUrl).map((response)=>response.json())
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response)=>response.json() as Album)
   }
 
 }
